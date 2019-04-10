62b88434b1312e5654d6966d7a3d81a3c818fdbf
Vladislav <vladislavnekhaichik@gmail.com>
2019-04-10 19:20:48 +0300

    first commit

---
+++ src/BlogNullReference.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogNullReference.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
