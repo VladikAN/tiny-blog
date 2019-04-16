5d7b53d12cecf694965f207d58ac19e067449cdd
Vladislav <vladislavnekhaichik@gmail.com>
2019-04-16 18:15:42 +0300

    added default service code

---
+++ src/BlogNullReference.DataServices/Services/BaseDataService.cs
@@ -0,0 +1,19 @@
+﻿using BlogNullReference.DataServices.Entities;
+using BlogNullReference.DataServices.Settings;
+using MongoDB.Driver;
+
+namespace BlogNullReference.DataServices.Services
+{
+    public abstract class BaseDataService<TEntity> where TEntity : BaseEntity
+    {
+        private const string DatabaseName = "blog";
+        protected IMongoClient Client;
+        protected IMongoDatabase Database;
+
+        protected BaseDataService(IDatabaseSettings settings)
+        {
+            Client = new MongoClient(settings.ConnectionString);
+            Database = Client.GetDatabase(DatabaseName);
+        }
+    }
+}
---
+++ src/BlogNullReference.DataServices/Services/PostDataService.cs
@@ -0,0 +1,12 @@
+﻿using BlogNullReference.DataServices.Entities;
+using BlogNullReference.DataServices.Settings;
+
+namespace BlogNullReference.DataServices.Services
+{
+    public class PostDataService : BaseDataService<Post>
+    {
+        public PostDataService(IDatabaseSettings databaseSettings) : base(databaseSettings)
+        {
+        }
+    }
+}
