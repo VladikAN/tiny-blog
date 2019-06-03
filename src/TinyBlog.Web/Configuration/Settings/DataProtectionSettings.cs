fe0a3353cd3be1cf19fc3557b423419edcbf3d1d
Vladislav <vladislavnekhaichik@gmail.com>
2019-06-03 17:31:01 +0300

    implemented data persistence and protection

---
+++ src/TinyBlog.Web/Configuration/Settings/DataProtectionSettings.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace TinyBlog.Web.Configuration.Settings
+{
+    public class DataProtectionSettings : IDataProtectionSettings
+    {
+        private const string SectionName = "DataProtection";
+
+        public DataProtectionSettings(IConfiguration configuration)
+        {
+            var section = configuration.GetSection(SectionName);
+            Enabled = section.GetValue<bool>(nameof(Enabled));
+            if (Enabled)
+            {
+                BlobUriWithSas = new Uri(
+                    section.GetValue<string>(nameof(BlobUriWithSas)),
+                    UriKind.Absolute);
+            }
+        }
+
+        public bool Enabled { get; }
+        public Uri BlobUriWithSas { get; }
+    }
+}
---
+++ src/TinyBlog.Web/Configuration/Settings/IDataProtectionSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TinyBlog.Web.Configuration.Settings
+{
+    public interface IDataProtectionSettings
+    {
+        bool Enabled { get; }
+        Uri BlobUriWithSas { get; }
+    }
+}
