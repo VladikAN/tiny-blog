ee7a90cc6ecbd5eb75bde35847038c65f40a696f
Vladislav <vladislavnekhaichik@gmail.com>
2019-06-27 16:07:04 +0300

    added redux stuff to work with layout

---
+++ src/TinyBlog.Web/Scripts/store/layout/reducers.ts
@@ -0,0 +1,48 @@
+import { Layout } from './types';
+import {
+    LayoutActionTypes,
+    GET_LAYOUT_STARTED_MESSAGE,
+    GET_LAYOUT_COMPLETED_MESSAGE,
+    SAVE_LAYOUT_STARTED_MESSAGE,
+    SAVE_LAYOUT_COMPLETED_MESSAGE
+} from './actions';
+import { 
+    REQUEST_FAILED_MESSAGE,
+    SharedActionTypes
+} from '../shared/actions';
+
+export interface LayoutState extends Layout {
+    isFetching: boolean;
+    isFetched: boolean;
+    isSaving: boolean;
+}
+
+const initialState: LayoutState = {
+    title: '',
+    description: '',
+    uri: '',
+    author: '',
+    language: '',
+    googleTagsCode: '',
+    footerContent: '',
+    isFetching: false,
+    isFetched: false,
+    isSaving: false
+};
+
+export function layoutReducer(state = initialState, action: LayoutActionTypes | SharedActionTypes): LayoutState {
+    switch (action.type) {
+        case REQUEST_FAILED_MESSAGE:
+            return { ...state, isFetching: false, isSaving: false };
+        case GET_LAYOUT_STARTED_MESSAGE:
+            return { ...state, isFetching: true, isFetched: false };
+        case GET_LAYOUT_COMPLETED_MESSAGE:
+            return { ...state, ...action.layout, isFetching: false, isFetched: true };
+        case SAVE_LAYOUT_STARTED_MESSAGE:
+            return { ...state, isSaving: true };
+        case SAVE_LAYOUT_COMPLETED_MESSAGE:
+            return { ...state, ...action.layout, isSaving: false };
+        default:
+            return state;
+    }
+}
