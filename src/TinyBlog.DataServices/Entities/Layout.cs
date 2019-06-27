e80f763dfc45ffce8b32f1ea3d761181a2f6b82e
Vladislav <vladislavnekhaichik@gmail.com>
2019-06-27 15:17:45 +0300

    added backend for site layout management

---
+++ src/TinyBlog.DataServices/Entities/Layout.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace TinyBlog.DataServices.Entities
+{
+    public class Layout : BaseEntity
+    {
+        [BsonElement("title")]
+        public string Title { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+        [BsonElement("uri")]
+        public string Uri { get; set; }
+
+        [BsonElement("author")]
+        public string Author { get; set; }
+
+        [BsonElement("language")]
+        public string Language { get; set; }
+
+        [BsonElement("googleTagsCode")]
+        public string GoogleTagsCode { get; set; }
+
+        [BsonElement("footerContent")]
+        public string FooterContent { get; set; }
+    }
+}
---
+++ src/TinyBlog.DataServices/Extensions/LayoutExtensions.cs
@@ -0,0 +1,44 @@
+﻿using TinyBlog.DataServices.Entities;
+using TinyBlog.DataServices.Services.Dto;
+
+namespace TinyBlog.DataServices.Extensions
+{
+    public static class LayoutExtensions
+    {
+        public static LayoutDto BuildDto(this Layout domain)
+        {
+            if (domain == null)
+            {
+                return null;
+            }
+
+            return new LayoutDto(
+                domain.Title,
+                domain.Description,
+                domain.Uri,
+                domain.Author,
+                domain.Language,
+                domain.GoogleTagsCode,
+                domain.FooterContent);
+        }
+
+        public static Layout BuildDomain(this LayoutDto dto)
+        {
+            if (dto == null)
+            {
+                return null;
+            }
+
+            return new Layout
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                Uri = dto.Uri,
+                Author = dto.Author,
+                Language = dto.Language,
+                GoogleTagsCode = dto.GoogleTagsCode,
+                FooterContent = dto.FooterContent
+            };
+        }
+    }
+}
